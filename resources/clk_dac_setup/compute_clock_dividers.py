d36e6e29fc41d39efeacdafb52fad7438f415693
lucianom <lucianom@xmos.com>
2020-09-18 11:50:40 +0100

    Check output clocks are equal to the desired values

--- resources/clk_dac_setup/compute_clock_dividers.py
+++ resources/clk_dac_setup/compute_clock_dividers.py
@@ -32,14 +32,22 @@ if __name__ == "__main__":
     [mclk_khz, mclk_div_i, mclk_div_f] = compute_dividers(args.source_clock_khz, args.desired_mclock_khz)
 
     # The MCLK must be an exact multiple of the BCLK to avoid glitches in the I2S audio
-    bclk_48khz = float(mclk_khz) / 8
-    [bclk_48khz, bclk_48khz_div_i, bclk_48khz_div_f] = compute_dividers(args.source_clock_khz, bclk_48khz)
+    desired_bclk_48khz = float(mclk_khz) / 8
+    [bclk_48khz, bclk_48khz_div_i, bclk_48khz_div_f] = compute_dividers(args.source_clock_khz, desired_bclk_48khz)
 
-    bclk_16khz = mclk_khz / 24
-    [bclk_16khz, bclk_16khz_div_i, bclk_16khz_div_f] = compute_dividers(args.source_clock_khz, bclk_16khz)
+    desired_bclk_16khz = mclk_khz / 24
+    [bclk_16khz, bclk_16khz_div_i, bclk_16khz_div_f] = compute_dividers(args.source_clock_khz, desired_bclk_16khz)
 
 
     print(f"Given source clock: {args.source_clock_khz}kHz the dividers are:")
     print(f"\tMCLK: freq {mclk_khz}kHz CLK_I {mclk_div_i} CLK_F {mclk_div_f}")
-    print(f"\tBCLK at 48kHz: freq {bclk_48khz}kHz CLK_I {bclk_48khz_div_i} CLK_F {bclk_48khz_div_f}")
-    print(f"\tBCLK at 16kHz: freq {bclk_16khz}kHz CLK_I {bclk_16khz_div_i} CLK_F {bclk_16khz_div_f}")
+    print(f"\tBCLK at 48kHz: freq {bclk_48khz}kHz, ratio {mclk_khz/bclk_48khz}, CLK_I {bclk_48khz_div_i}, CLK_F {bclk_48khz_div_f}")
+    print(f"\tBCLK at 16kHz: freq {bclk_16khz}kHz, ratio {mclk_khz/bclk_16khz}, CLK_I {bclk_16khz_div_i} CLK_F {bclk_16khz_div_f}")
+    if int(args.desired_mclock_khz) != mclk_khz:
+        print(f"Warning: perfect value for MCLK couldn't be found: expected {args.desired_mclock_khz}, found  {mclk_khz}\n")
+    if desired_bclk_48khz != bclk_48khz:
+        print(f"Warning: perfect value for BCLK at 48kHz couldn't be found: expected {desired_bclk_48khz}, found  {desired_bclk_48khz}\n")
+    if desired_bclk_16khz != bclk_16khz:
+        print(f"Warning: perfect value for BCLK at 16kHz couldn't be found: expected {desired_bclk_16khz}, found  {desired_bclk_16khz}")
+
+
