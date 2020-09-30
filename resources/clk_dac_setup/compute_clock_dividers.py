b773b1be86ee38aa3814f636dbab6555e4f6e1ae
lucianom <lucianom@xmos.com>
2020-09-30 10:17:25 +0100

    Add mclk_pdm_clk_divider argument and improve execution speed

--- resources/clk_dac_setup/compute_clock_dividers.py
+++ resources/clk_dac_setup/compute_clock_dividers.py
@@ -5,37 +5,42 @@ def compute_dividers(source_clock_khz, output_clock_khz):
     final_div_i = 0;
     final_div_f = 0;
     final_output_clock_khz = 0
-    for div_i in range(1, 2000):
-        for div_f in range(0, 4096):
-            computed_output_clock_khz = float(source_clock_khz)*1000/(div_i + div_f/4096)/1000
-            new_diff = abs(computed_output_clock_khz - float(output_clock_khz))
-            # search for the minimum difference between the desired frequency and the one achievable with the dividers
-            if new_diff < diff:
-                diff = new_diff
-                final_div_i = div_i
-                final_div_f = div_f
-                final_output_clock_khz = computed_output_clock_khz
-                if diff == 0:
-                    break
+    div_i = int(source_clock_khz / output_clock_khz)
+    for div_f in range(0, 4096):
+        computed_output_clock_khz = float(source_clock_khz)*1000/(div_i + div_f/4096)/1000
+        new_diff = abs(computed_output_clock_khz - float(output_clock_khz))
+        # search for the minimum difference between the desired frequency and the one achievable with the dividers
+        if new_diff < diff:
+            diff = new_diff
+            final_div_i = div_i
+            final_div_f = div_f
+            final_output_clock_khz = computed_output_clock_khz
+            if diff == 0:
+                break
     return [final_output_clock_khz, final_div_i, final_div_f]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('source_clock_khz',
+    parser.add_argument('source_clock_khz', type=int,
                         help="Source clock frequency in kHz")
 
-    parser.add_argument('desired_mclock_khz',
+    parser.add_argument('desired_mclock_khz', type=int,
                         help="Desired frequency of MCLK in kHz")
+
+    parser.add_argument('--mclk_pdm_clk_divider', '-d', type=int, default=2, choices=[1, 2],
+                        help="XCore divider from input master clock to 6.144MHz DDR PDM microphone clock")
 
     args = parser.parse_args()
 
     [mclk_khz, mclk_div_i, mclk_div_f] = compute_dividers(args.source_clock_khz, args.desired_mclock_khz)
 
+    mclk_bclk_ratio = 4 * args.mclk_pdm_clk_divider
+
     # The MCLK must be an exact multiple of the BCLK to avoid glitches in the I2S audio
-    desired_bclk_48khz = float(mclk_khz) / 8
+    desired_bclk_48khz = float(mclk_khz) / mclk_bclk_ratio
     [bclk_48khz, bclk_48khz_div_i, bclk_48khz_div_f] = compute_dividers(args.source_clock_khz, desired_bclk_48khz)
 
-    desired_bclk_16khz = mclk_khz / 24
+    desired_bclk_16khz = mclk_khz / (mclk_bclk_ratio * 3)
     [bclk_16khz, bclk_16khz_div_i, bclk_16khz_div_f] = compute_dividers(args.source_clock_khz, desired_bclk_16khz)
 
 
