6eb12cb409b99c38ea58df1fb12084a9e8c73b87
LeslieXMOS <leslielee@xmos.com>
2025-03-07 11:39:58 +0800

    Update: Fix i2s_driver_script

--- setup.sh
+++ setup.sh
@@ -252,9 +252,10 @@ if [[ -n "$I2S_MODE" ]]; then
   I2S_NAME=i2s_$I2S_MODE
   I2S_MODULE=$RPI_SETUP_DIR/loader/$I2S_NAME/${I2S_NAME}_loader.ko
   echo "if sudo insmod $I2S_MODULE ; then"                  >> $i2s_driver_script
-  echo "  pushd $I2S_BUILD_DIR > /dev/null"                 >> $i2s_driver_script
-  echo "  eval $CMD"                                        >> $i2s_driver_script
-  echo "  popd > /dev/null"                                 >> $i2s_driver_script
+  echo "  echo $I2S_MODULE loaded successfully"             >> $i2s_driver_script
+  echo "else"                                               >> $i2s_driver_script
+  echo "  cd $I2S_BUILD_DIR"                                >> $i2s_driver_script
+  echo "  $CMD"                                             >> $i2s_driver_script
   echo "  sudo insmod $I2S_MODULE"                          >> $i2s_driver_script
   echo "fi"                                                 >> $i2s_driver_script
 
